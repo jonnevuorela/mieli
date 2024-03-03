ef22c601286602f708167bd968335c351a871f98
Jonne Vuorela <vuorelajonne@gmail.com>
2024-03-03 16:21:30 +0200

    1

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { appWindow } from '@tauri-apps/api/window'
-
-document.getElementById('titlebar-minimize').addEventListener('click', () => appWindow.minimize())
-document.getElementById('titlebar-maximize').addEventListener('click', () => appWindow.toggleMaximize())
-document.getElementById('titlebar-close').addEventListener('click', () => appWindow.close())
-
-document.getElementById('addNew').addEventListener('click', () => {window.__TAURI__.invoke('addNewDiv');});
